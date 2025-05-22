828753e75075f5d7235503bfbcdd1cb80809b2f4
sejinbaek <sejin9767@gmail.com>
2025-05-22 17:11:35 +0900

    feat: 로그인 form 유효성 검사 및 백엔드로 입력 데이터 전송

--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -6,3 +6,8 @@ export const registerUser = async userData => {
   const response = await axios.post(`${API_URL}/register`, userData)
   return response.data
 }
+
+export const loginUser = async credentials => {
+  const response = await axios.post(`${API_URL}/login`, credentials)
+  return response.data
+}
