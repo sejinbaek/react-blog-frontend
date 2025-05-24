21453a32ede39b60b532f1d82e1e91f1e6a13fea
sejinbaek <sejin9767@gmail.com>
2025-05-24 10:37:20 +0900

    feat: 무한 스크롤 적용

--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import css from './postlistpage.module.css'
 import PostCard from '../components/PostCard'
 import { getPostList } from '../apis/postApi'
@@ -10,23 +10,46 @@ const PostListPage = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState(null)
 
+  // 페이지네이션 상태 관리
+  const [page, setPage] = useState(0)
+  const [hasMore, setHasMore] = useState(true)
+  const listRef = useRef(null)
+  const observer = useRef()
+
+  const lastPostElementRef = useCallback(
+    node => {
+      if (isLoading || !node) return
+      if (observer.current) observer.current.disconnect()
+
+      observer.current = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage(prev => prev + 1)
+        }
+      })
+      observer.current.observe(node)
+    },
+    [isLoading, hasMore]
+  )
+
   useEffect(() => {
     const fetchPostList = async () => {
       try {
-        setIsLoading(true)
-        const data = await getPostList()
-        console.log('목록조회 성공:', data)
-        setPostList(data.posts)
+        // 페이지가 0보다 크면 추가 데이터 로딩
+        if (page > 0) setIsLoading(true)
+        // 수정된 페이지 정보 전달
+        const data = await getPostList(page)
+
+        setPostList(prev => (page === 0 ? data.posts : [...prev, ...data.posts]))
+        setHasMore(data.hasMore)
       } catch (error) {
         console.error('목록조회 실패:', error)
-        setError('글 목록을 불러오는 데 실패했습니다')
+        setError('글 목록을 불러오는데 실패했습니다.')
       } finally {
         setIsLoading(false)
       }
     }
-
     fetchPostList()
-  }, [])
+  }, [page])
 
   return (
     <main className={css.postlistpage}>
@@ -39,9 +62,9 @@ const PostListPage = () => {
       ) : postList.length === 0 ? (
         <p className={css.noPostMessage}>첫 번째 글의 주인공이 되어주세요</p>
       ) : (
-        <ul className={css.postList}>
-          {postList.map(post => (
-            <li key={post._id}>
+        <ul className={css.postList} ref={listRef}>
+          {postList.map((post, i) => (
+            <li key={post._id} ref={i === postList.length - 1 ? lastPostElementRef : null}>
               <PostCard post={post} />
             </li>
           ))}
