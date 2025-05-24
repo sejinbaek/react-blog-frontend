2dd31f77128a35f52729e573a94884e8d1c3e31d
sejinbaek <sejin9767@gmail.com>
2025-05-24 11:24:20 +0900

    feat: 포스트 클릭 시 포스트 디테일 페이지로 이동

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,18 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import css from './postcard.module.css'
 import { formatDate } from '../utils/features'
 
 const PostCard = ({ post }) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/post/${post._id}`)
+  }
   console.log(post)
   return (
-    <article className={css.postcard}>
+    <article className={css.postcard} onClick={handleClick}>
       <div className={css.post_img}>
         <img src={`${import.meta.env.VITE_BACK_URL}/${post.cover}`} alt={post.title} />
       </div>
       <h3 className={css.title}>{post.title}</h3>
       <div classname={css.info}>
         <p>
-          <Link to={`/mypage`} className={css.author}>
+          <Link to={`/mypage`} className={css.author} onClick={e => e.stopPropagation()}>
             {post.author}
           </Link>
           <time className={css.date}>{formatDate(post.createdAt)}</time>
