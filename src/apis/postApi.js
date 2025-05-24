5caced8415e128db0f7f711ecd108c1373227ec0
sejinbaek <sejin9767@gmail.com>
2025-05-24 09:26:33 +0900

    feat: 글 목록 조회 API 로직 구현 (페이지네이션 지원)

--- src/apis/postApi.js
+++ src/apis/postApi.js
@@ -7,3 +7,8 @@ export const createPost = async postData => {
   const response = await axios.post(`${API_URL}/postWrite`, postData)
   return response.data
 }
+
+export const getPostList = async (page = 0, limit = 3) => {
+  const response = await axios.get(`${API_URL}/postList`, { params: { page, limit } })
+  return response.data
+}
