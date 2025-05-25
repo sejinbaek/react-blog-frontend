911d8e47154e8f11f20d22972462d44e337766d5
sejinbaek <sejin9767@gmail.com>
2025-05-25 09:34:23 +0900

    feat: 포스트 상세 조회 및 삭제 API 구현

--- src/apis/postApi.js
+++ src/apis/postApi.js
@@ -3,12 +3,26 @@ axios.defaults.withCredentials = true
 
 const API_URL = import.meta.env.VITE_BACK_URL
 
+// 포스트 등록 API
 export const createPost = async postData => {
   const response = await axios.post(`${API_URL}/postWrite`, postData)
   return response.data
 }
 
+// 포스트 리스트 조회 API
 export const getPostList = async (page = 0, limit = 3) => {
   const response = await axios.get(`${API_URL}/postList`, { params: { page, limit } })
+  return response.data
+}
+
+// 포스트 상세 조회 API
+export const getPostDetail = async postId => {
+  const response = await axios.get(`${API_URL}/post/${postId}`)
+  return response.data
+}
+
+// 포스트 삭제 API
+export const deletePost = async postId => {
+  const response = await axios.delete(`${API_URL}/post/${postId}`)
   return response.data
 }
