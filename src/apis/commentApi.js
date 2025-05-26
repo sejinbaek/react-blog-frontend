fda5ddda151c83452ae02636561cd2a7496a923a
sejinbaek <sejin9767@gmail.com>
2025-05-26 09:45:08 +0900

    feat: comment 조회 API 로직 구현

--- src/apis/commentApi.js
+++ src/apis/commentApi.js
@@ -2,8 +2,14 @@ import axios from 'axios'
 axios.defaults.withCredentials = true
 const API_URL = import.meta.env.VITE_BACK_URL
 
-// 댓글 작성 API
+// 댓글 등록 API
 export const createComment = async commentData => {
   const response = await axios.post(`${API_URL}/comments`, commentData)
+  return response.data
+}
+
+// 댓글 조회 API
+export const getComments = async postId => {
+  const response = await axios.get(`${API_URL}/comments/${postId}`)
   return response.data
 }
