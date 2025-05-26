9dcd1663eb0d06570163fc1433df59e8f3e02cd3
sejinbaek <sejin9767@gmail.com>
2025-05-26 11:49:57 +0900

    feat: 마이페이지 API 로직 구현
    - 사용자 정보 조회 API 구현
    - 사용자 작성 글 조회 API 구현
    - 사용자 작성 댓글 API 구현
    - 사용자가 좋아요 누른 포스트 조회 API 구현

---
+++ src/apis/userInfoApi.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+axios.defaults.withCredentials = true
+const API_URL = import.meta.env.VITE_BACK_URL || 'http://localhost:3000'
+
+// 특정 사용자 정보 조회
+export const getUserInfo = async username => {
+  try {
+    const response = await axios.get(`${API_URL}/user/${username}`)
+    return response.data
+  } catch (error) {
+    console.error('사용자 정보 조회 실패:', error)
+    throw error
+  }
+}
+
+// 특정 사용자가 작성한 글 조회
+export const getUserPosts = async username => {
+  try {
+    const response = await axios.get(`${API_URL}/user/${username}/posts`)
+    return response.data
+  } catch (error) {
+    console.error('사용자 게시물 조회 싥패:', error)
+    throw error
+  }
+}
+
+// 특정 사용자가 작성한 댓글 조회
+export const getUserComments = async username => {
+  try {
+    const response = await axios.get(`${API_URL}/user/${username}/comments`)
+    return response.data
+  } catch (error) {
+    console.error('사용자 댓글 조회 실패', error)
+    throw error
+  }
+}
+
+// 특정 사용자가 좋아요 클릭한 글 조회
+export const getUserLikes = async username => {
+  try {
+    const response = await axios.get(`${API_URL}/user/${username}/likes`)
+    return response.data
+  } catch (error) {
+    console.error('사용자 좋아요 게시물 조회 실패:', error)
+    throw error
+  }
+}
